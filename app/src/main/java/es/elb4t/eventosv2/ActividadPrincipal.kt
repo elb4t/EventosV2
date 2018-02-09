1b714f30e97532659d0be3f2d77db1d7292078ea
Elb4t <eloy4t@gmail.com>
2018-02-09 22:44:54 +0100

    Aplicación creada

---
+++ app/src/main/java/es/elb4t/eventosv2/ActividadPrincipal.kt
@@ -0,0 +1,12 @@
+package es.elb4t.eventosv2
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ActividadPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_principal)
+    }
+}
