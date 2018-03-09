44d4df3ba65273519b1803520281d661487bcffe
Elb4t <eloy4t@gmail.com>
2018-03-09 23:19:41 +0100

    Crear carpetas en Google Drive.

--- app/src/main/java/es/elb4t/eventosv2/FotografiasDrive.kt
+++ app/src/main/java/es/elb4t/eventosv2/FotografiasDrive.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
@@ -17,9 +18,12 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.api.client.extensions.android.http.AndroidHttp
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
+import com.google.api.services.drive.model.File
+import java.io.IOException
 import java.util.*
 
 
@@ -44,6 +48,9 @@ class FotografiasDrive : AppCompatActivity() {
     val SOLICITUD_HACER_FOTOGRAFIA = 4
     private val uriFichero: Uri? = null
 
+    private var idCarpeta:String? = ""
+    private var idCarpetaEvento:String? = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fotografias_drive)
@@ -57,12 +64,17 @@ class FotografiasDrive : AppCompatActivity() {
         val prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE)
         nombreCuenta = prefs.getString("nombreCuenta", null)
         noAutoriza = prefs.getBoolean("noAutoriza", false)
+        idCarpeta = prefs.getString("idCarpeta", null)
+        idCarpetaEvento = prefs.getString("idCarpeta_$evento", null)
         if (!noAutoriza) {
             if (nombreCuenta == null) {
                 PedirCredenciales()
             } else {
                 credencial.selectedAccountName = nombreCuenta
                 servicio = obtenerServicioDrive(credencial)
+                if (idCarpetaEvento == null) {
+                    crearCarpetaEnDrive(evento!!, idCarpeta)
+                }
             }
         }
     }
@@ -74,7 +86,7 @@ class FotografiasDrive : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val vista = findViewById<View>(android.R.id.content) as View
-        val id = item.getItemId()
+        val id = item.itemId
         when (id) {
             R.id.action_camara -> {
             }
@@ -113,13 +125,14 @@ class FotografiasDrive : AppCompatActivity() {
                         && data.extras != null) {
                     nombreCuenta = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
                     if (nombreCuenta != null) {
-                        credencial.setSelectedAccountName(nombreCuenta)
+                        credencial.selectedAccountName = nombreCuenta
                         servicio = obtenerServicioDrive(credencial)
                         val prefs = getSharedPreferences(
                                 "Preferencias", Context.MODE_PRIVATE)
                         val editor = prefs.edit()
                         editor.putString("nombreCuenta", nombreCuenta)
                         editor.commit()
+                        crearCarpetaEnDrive(evento!!, idCarpeta)
                     }
                 }
             }
@@ -128,11 +141,70 @@ class FotografiasDrive : AppCompatActivity() {
             SOLICITUD_SELECCIONAR_FOTOGRAFIA -> {
             }
             SOLICITUD_AUTORIZACION -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    crearCarpetaEnDrive(evento!!, idCarpeta)
+                } else {
+                    noAutoriza = true;
+                    var prefs: SharedPreferences = getSharedPreferences("Preferencias", Context.MODE_PRIVATE)
+                    var editor: SharedPreferences.Editor = prefs.edit()
+                    editor.putBoolean("noAutoriza", true)
+                    editor.commit()
+                    mostrarMensaje(this, "El usuario no autoriza usar Google Drive")
+                }
             }
         }
     }
 
     private fun obtenerServicioDrive(credential: GoogleAccountCredential): Drive {
         return Drive.Builder(AndroidHttp.newCompatibleTransport(), GsonFactory(), credential).build()
+    }
+
+    private fun crearCarpetaEnDrive(nombreCarpeta: String, carpetaPadre: String?) {
+        var t: Thread = Thread(Runnable {
+            try {
+                var idCarpetaPadre = carpetaPadre
+                mostrarCarga(this@FotografiasDrive, "Creando carpeta...")
+                //Crear carpeta EventosDrive
+                if (idCarpeta == null) {
+                    val metadataFichero = File()
+                    metadataFichero.name = "EventosDrive"
+                    metadataFichero.mimeType = "application/vnd.google-apps.folder"
+                    val fichero = servicio!!.files().create(metadataFichero)
+                            .setFields("id").execute()
+                    if (fichero.id != null) {
+                        val prefs = getSharedPreferences("Preferencias",
+                                Context.MODE_PRIVATE)
+                        val editor = prefs.edit()
+                        editor.putString("idCarpeta", fichero.id)
+                        editor.commit()
+                        idCarpetaPadre = fichero.id
+                    }
+                }
+                val metadataFichero = File()
+                metadataFichero.name = nombreCarpeta
+                metadataFichero.mimeType = "application/vnd.google-apps.folder"
+                if (idCarpetaPadre != "") {
+                    metadataFichero.parents = Collections.singletonList(idCarpetaPadre)
+                }
+                val fichero = servicio!!.files().create(metadataFichero).setFields("id").execute()
+                if (fichero.id != null) {
+                    val prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE)
+                    var editor: SharedPreferences.Editor = prefs.edit()
+                    editor.putString("idCarpeta_" + evento, fichero.id)
+                    editor.commit()
+                    idCarpetaEvento = fichero.id
+                    mostrarMensaje(this@FotografiasDrive, "¡Carpeta creada!")
+                }
+                ocultarCarga(this@FotografiasDrive)
+            } catch (e: UserRecoverableAuthIOException) {
+                ocultarCarga(this@FotografiasDrive)
+                startActivityForResult(e.intent, SOLICITUD_AUTORIZACION)
+            } catch (e: IOException) {
+                mostrarMensaje(this@FotografiasDrive, "Error; ${e.message}")
+                ocultarCarga(this@FotografiasDrive)
+                e.printStackTrace()
+            }
+        })
+        t.start()
     }
 }
